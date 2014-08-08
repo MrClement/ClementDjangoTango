6545959fd70a4df1360ab5421b93d4944ede436d
MrClement <aclement@kentdenver.org>
2014-08-07 21:10:03 -0600

    stuff

---
+++ test.py
@@ -0,0 +1,13 @@
+__author__ = 'aclement'
+
+from django.conf import settings
+import os
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'DjangoTango.settings')
+path = os.path.join(settings.BASE_DIR, "static")
+file_name = os.path.join(path, "CompletedCourses.csv")
+
+file = open(file_name)
+
+for line in file:
+    print line
